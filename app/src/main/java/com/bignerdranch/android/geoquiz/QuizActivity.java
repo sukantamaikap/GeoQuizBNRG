a21f11b71003f072f3f2bc04eea4c1cc8b8e5601
sukantamaikap <sukanta.maikap@gmail.com>
2016-06-11 12:47:19 +0530

    end of chapter 1

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,29 +1,36 @@
 package com.bignerdranch.android.geoquiz;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        this.mTrueButton = (Button) findViewById(R.id.true_button);
+        this.mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick (View v) {
+                Toast.makeText(QuizActivity.this, R.string.correct_text, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        this.mFalseButton = (Button) findViewById(R.id.false_button);
+        this.mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_text, Toast.LENGTH_LONG).show();
             }
         });
     }
